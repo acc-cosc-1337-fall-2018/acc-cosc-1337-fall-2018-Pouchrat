73ee981589834dc6c93b9e234d4eef165dfdf734
Nat Arney <38634418+Pouchrat@users.noreply.github.com>
2018-09-13 19:08:28 -0500

    Update basics_test.cpp

--- 01-basics_test/basics_test.cpp
+++ 01-basics_test/basics_test.cpp
@@ -20,3 +20,15 @@ TEST_CASE("Test multiply_numbers", "Products 4,25,100")
 	REQUIRE(multiply_numbers(5, 5) == 25);
 	REQUIRE(multiply_numbers(10, 10) == 100);
 }
+
+TEST_CASE("test pass by val and ref", "(0,0) (0, 99)")
+{
+	int num1{0};
+	int num2{0};
+	
+	pass_by_val_by_ref(num1, num2);
+	
+	REQUIRE(num1 == 0);
+	REQUIRE(num2 == 99);
+	
+}
