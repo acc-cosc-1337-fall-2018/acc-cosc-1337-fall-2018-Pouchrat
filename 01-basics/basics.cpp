5975622d31eb58dde5933f15d57eb76c69fd4195
Nat Arney <38634418+Pouchrat@users.noreply.github.com>
2018-09-13 19:04:27 -0500

    Update basics.cpp

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -1,13 +1,5 @@
 #include "basics.h"
 
-#include "references.h"
-
-void pass_by_val_by_ref(int val, int& ref)
-{
-	val = 100;
-	ref = 99;
-}
-
 int sum_numbers(int num1, int num2)
 {
 	return num1 + num2;
@@ -18,4 +10,10 @@ int sum_numbers(int num1, int num2)
 int multiply_numbers(int num1, int num2)
 {
 	return num1 * num2;
+}
+
+void pass_by_val_by_ref(int val, int& ref)
+{
+	val = 100;
+	ref = 99;
 }
