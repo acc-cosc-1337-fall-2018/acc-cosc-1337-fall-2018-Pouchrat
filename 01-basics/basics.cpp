fbaf0b6f187fbe0ff1889b23c3c3ac989064b506
Nat Arney <38634418+Pouchrat@users.noreply.github.com>
2018-09-13 19:04:02 -0500

    Update basics.cpp

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -1,5 +1,13 @@
 #include "basics.h"
 
+#include "references.h"
+
+void pass_by_val_by_ref(int val, int& ref)
+{
+	val = 100;
+	ref = 99;
+}
+
 int sum_numbers(int num1, int num2)
 {
 	return num1 + num2;
