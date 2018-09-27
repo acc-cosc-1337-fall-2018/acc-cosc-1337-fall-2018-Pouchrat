e6c7edee6881bbfbc242f98e3139295a73f0811c
Lux <Lux@DESKTOP-BTNMMFN>
2018-09-27 18:52:39 -0500

    classes_library typo fixed

--- assignments/05-classes/invoice_detail.h
+++ assignments/05-classes/invoice_detail.h
@@ -5,10 +5,10 @@
 using std::cout; using std::cin; using std::string; using std::vector;
 
 //class start
-#ifndef CLASS_NAME
+/*#ifndef CLASS_NAME
 #define CLASS_NAME
 
 
 
-#endif //CLASS_NAME
+#endif //CLASS_NAME*/
 //class end
